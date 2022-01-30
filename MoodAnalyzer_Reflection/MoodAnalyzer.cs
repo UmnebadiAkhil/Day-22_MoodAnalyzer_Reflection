4aa6484d73aa247f1a8361bd18be28e050c4a69b
Akhil <akhilu5042@gmail.com>
2022-01-30 20:04:03 +0530

    MoodAnalyzer_Reflection Project

---
+++ MoodAnalyzer_Reflection/MoodAnalyzer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer_Reflection
+{
+    public class MoodAnalyser
+    {
+        public string message;
+        //public MoodAnalyser()
+        //{
+        //}
+
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood()
+        {
+            string mood;
+            if (message == "I am in a Happy mood")
+                mood = "Happy";
+            else if (message == "I am in a Sad mood")
+                mood = "Sad";
+            else
+                mood = null;
+            return mood;
+        }
+    }
+}
---
+++ MoodAnalyzer_Reflection/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MoodAnalyzer_Reflection
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser App!");
+        }
+    }
+    }
+}
